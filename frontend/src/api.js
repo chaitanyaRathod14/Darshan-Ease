e50e0e5adea581b21b59d7b8dfb473bd93ff4ea0
Chaitnyarathod <rathodchaitanya830@gmail.com>
2026-06-26 16:37:34 +0530

    fix api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
-
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' 
+});
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
